3ba8bffa32dce2a7e821f4136a5eb960fbe81a98
pastoreekahk96 <apastoreekahk96@gmail.com>
2025-06-30 15:34:52 +0000

    Created magical database schema for ALX Online Bookstore

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,19 +1,17 @@
 -- Create the database
-CREATE DATABASE alx_book_store;
-
--- Use the database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 -- Create Authors table
 CREATE TABLE Authors (
-    author_id INT PRIMARY KEY,
-    author_name VARCHAR(215)
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
 );
 
 -- Create Books table
 CREATE TABLE Books (
-    book_id INT PRIMARY KEY,
-    title VARCHAR(130),
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE,
     publication_date DATE,
@@ -22,15 +20,15 @@ CREATE TABLE Books (
 
 -- Create Customers table
 CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(215),
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215),
     address TEXT
 );
 
 -- Create Orders table
 CREATE TABLE Orders (
-    order_id INT PRIMARY KEY,
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
@@ -38,7 +36,7 @@ CREATE TABLE Orders (
 
 -- Create Order_Details table
 CREATE TABLE Order_Details (
-    orderdetailid INT PRIMARY KEY,
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
